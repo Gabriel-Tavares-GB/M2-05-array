92876b21a4811d78f7c1f32885fb2a504508f4e8
Gabriel-Tavares-GB <gbtav83@gmail.com>
2021-05-26 15:10:16 -0300

    Add exercício Array

---
+++ script.js
@@ -0,0 +1,70 @@
+// Crie um array que receba 5 itens e exiba no console.
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+
+// Utilize um método para remover o último nome do array.
+
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+
+// Utilize um método para remover o primeiro nome do array.
+
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+
+
+
+/*
+let itens = ['Chapéu', 'Sapato', 'Roupa', 'Meia', 'Cinto'];
+console.log(itens);
+*/
+
+
+
+/*
+let itens = ['Chapéu', 'Sapato', 'Roupa', 'Meia', 'Cinto'];
+console.log(itens)
+
+var adiciona_início = itens.unshift('Camisa')
+console.log(itens)
+*/
+
+
+/*
+let itens = ['Chapéu', 'Sapato', 'Roupa', 'Meia', 'Cinto'];
+console.log(itens)
+
+var remove_ultimo = itens.pop();
+console.log(itens)
+*/
+
+/*
+let itens = ['Chapéu', 'Sapato', 'Roupa', 'Meia'];
+console.log(itens)
+
+var adiciona_final = itens.push('Calça', 'Bermuda');
+console.log(itens)
+*/
+
+
+/*let itens = ['Chapéu', 'Sapato', 'Roupa', 'Meia', 'Cinto'];
+console.log(itens);
+
+var remove_início = itens.shift();
+console.log(itens)
+*/
+
+
+
+/*
+const numbers = [7,5,6,3,8,9,2,1,4,20];
+numbers.sort((a,b) => a-b);
+console.log(numbers)
+*/
+
+
+
